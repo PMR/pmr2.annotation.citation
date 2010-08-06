f009abccf05a5f7b2435e0b593ad7eedebba9209
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-08-06 04:35:56 +0000

    Plone 4 related fixes.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.annotation.citation/trunk@5227 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/annotation/citation/tests/base.py
+++ pmr2/annotation/citation/tests/base.py
@@ -1,5 +1,8 @@
+from Testing import ZopeTestCase as ztc
+from Products.Five import zcml
+from Products.Five import fiveconfigure
 from Products.PloneTestCase.layer import onsetup, onteardown
-from pmr2.app.tests import base
+from Products.PloneTestCase import PloneTestCase as ptc
 
 @onsetup
 def setup():
@@ -7,6 +10,12 @@ def setup():
     fiveconfigure.debug_mode = True
     zcml.load_config('configure.zcml', pmr2.annotation.citation)
     fiveconfigure.debug_mode = False
-    base.ztc.installPackage('pmr2.annotation.citation')
+    ztc.installPackage('pmr2.annotation.citation')
 
-base.ptc.setupPloneSite(products=('pmr2.annotation.citation',))
+@onteardown
+def teardown():
+    pass
+
+setup()
+teardown()
+ptc.setupPloneSite(products=('pmr2.annotation.citation', 'pmr2.app'))
