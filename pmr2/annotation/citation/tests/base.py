b42cc62eebdb78d40ae6496875bad6816269a4b7
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2011-05-30 07:18:43 +0000

    Fixed the failing test case due to wysiwyg widget.
      - Plone 4 changed the default wysiwyg widget which broke tests, so
        in our simplified tests we use the simpilified parent testarea.  As
        the integration tests here only test the core functions this should
        result in any missed tests.  The actual wysiwyg part we don't need
        to care about here but can be tested through actual testing.
      - The fix is basically apply the testarea widget to the custom PMR2
        test layer for the wysiwyg widget.
    
    Progress towards:
    https://tracker.physiomeproject.org/show_bug.cgi?id=2937
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.annotation.citation/trunk@5882 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/annotation/citation/tests/base.py
+++ pmr2/annotation/citation/tests/base.py
@@ -3,12 +3,15 @@ from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Products.PloneTestCase.layer import onsetup, onteardown
 from Products.PloneTestCase import PloneTestCase as ptc
+import pmr2.testing
 
 @onsetup
 def setup():
     import pmr2.annotation.citation
     fiveconfigure.debug_mode = True
+    zcml.load_config('test.zcml', pmr2.testing)
     zcml.load_config('configure.zcml', pmr2.annotation.citation)
+    zcml.load_config('tests/test.zcml', pmr2.annotation.citation)
     fiveconfigure.debug_mode = False
     ztc.installPackage('pmr2.annotation.citation')
 
