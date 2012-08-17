26ba43794b2a01dfad68d425212e14be7813f1de
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-08-17 18:13:28 +1200

    Removal of the layout wrapper.

--- pmr2/annotation/citation/browser/form.py
+++ pmr2/annotation/citation/browser/form.py
@@ -1,12 +1,10 @@
 import zope.component
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
-from plone.z3cform import layout
 from plone.app.z3cform.wysiwyg.widget import WysiwygFieldWidget
 import z3c.form
 from z3c.form.interfaces import INPUT_MODE
 
 from pmr2.app.browser import form
-from pmr2.app.browser.layout import PlainLayoutWrapper
 from pmr2.app.browser.interfaces import IObjectIdMixin
 
 from pmr2.app.exposure.browser.browser import ExposureFileViewBase
@@ -36,9 +34,6 @@ class LicenseAddForm(form.AddForm):
         ctxobj.setTitle(self._data['title'])
         ctxobj.license_uri = self._data['license_uri']
 
-LicenseAddFormView = layout.wrap_form(LicenseAddForm, 
-    __wrapper_class=PlainLayoutWrapper)
-
 
 class LicenseEditForm(form.EditForm):
     """\
@@ -51,14 +46,8 @@ class LicenseEditForm(form.EditForm):
     )
     fields['portlet_text'].widgetFactory[INPUT_MODE] = WysiwygFieldWidget
 
-LicenseEditFormView = layout.wrap_form(LicenseEditForm, 
-    __wrapper_class=PlainLayoutWrapper)
-
 
 class LicenseCitationNote(ExposureFileViewBase):
     """\
     The citation viewer.
     """
-
-LicenseCitationNoteView = layout.wrap_form(LicenseCitationNote, 
-    __wrapper_class=PlainLayoutWrapper)
--- pmr2/annotation/citation/browser/view.py
+++ pmr2/annotation/citation/browser/view.py
@@ -1,10 +1,8 @@
 import zope.component
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
-from plone.z3cform import layout
 from Products.CMFCore.utils import getToolByName
 
 from pmr2.app.settings.interfaces import IPMR2GlobalSettings
-from pmr2.app.browser.layout import PlainLayoutWrapper
 
 from pmr2.app.exposure.browser.browser import ExposureFileViewBase
 
@@ -28,10 +26,3 @@ class LicenseCitationNote(ExposureFileViewBase):
         pmr2 = zope.component.getUtility(IPMR2GlobalSettings)
         settings = IPluginSettings(pmr2)
         return settings.citation_instruction
-
-    def __call__(self):
-        self.update()
-        return self.template()
-
-LicenseCitationNoteView = layout.wrap_form(LicenseCitationNote, 
-    __wrapper_class=PlainLayoutWrapper)
