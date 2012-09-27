a94c45d2ab64e32eb47b64eaee04d51d5f58d121
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-09-27 13:10:49 +1200

    Fix deprecation warnings.

--- pmr2/annotation/citation/tests/test_doctest.py
+++ pmr2/annotation/citation/tests/test_doctest.py
@@ -1,12 +1,10 @@
 import unittest
+import doctest
 
-from zope.testing import doctestunit, doctest
 from zope.component import testing
 from Testing import ZopeTestCase as ztc
 
 from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite
-from Products.PloneTestCase.layer import onsetup
 
 from pmr2.annotation.citation.tests import base
 
